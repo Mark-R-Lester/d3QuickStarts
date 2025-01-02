0ac0841d5f24ce6ad15a6ddf2c487af7032d3f9c
mark-r-lester <mrlester@live.co.uk>
2025-01-02 10:53:32 +0000

    Adding missed file

--- quick-starts/src/radial/radialText/types.ts
+++ quick-starts/src/radial/radialText/types.ts
@@ -6,11 +6,6 @@ import {
   QsEnumTextAnchor,
 } from '../../core/enums/qsEnums'
 
-export interface QsValuedText {
-  value: number
-  text?: string
-}
-
 export interface RadialTextConfigStrict {
   [key: string]: number | Iterable<unknown> | Iterable<string> | undefined
   radius: number

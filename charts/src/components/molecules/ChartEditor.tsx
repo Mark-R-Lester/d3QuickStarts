9556d55558d0859ed91ca345e60f40958f9f8778
mark-r-lester <mrlester@live.co.uk>
2025-06-07 15:23:52 +0100

    Enable the use of QsEmun types in the chart editor

--- charts/src/components/molecules/ChartEditor.tsx
+++ charts/src/components/molecules/ChartEditor.tsx
@@ -5,7 +5,21 @@ import 'ace-builds/src-noconflict/theme-monokai'
 import 'ace-builds/src-noconflict/worker-javascript'
 import ace from 'ace-builds'
 import { transpile } from 'typescript'
-import { qsCreateCanvas } from 'd3qs/d3QuickStart'
+import {
+  qsCreateCanvas,
+  QsEnumAxisScaleType,
+  QsEnumColorScale,
+  QsEnumCurve,
+  QsEnumAlignmentBaseline,
+  QsEnumTextAnchor,
+  QsEnumTextFontWeight,
+  QsEnumTextFontStyle,
+  QsEnumTextDecorationLine,
+  QsEnumTextFont,
+  QsEnumLineJoin,
+  QsEnumLineCap,
+  QsEnumScaleType,
+} from 'd3qs/d3QuickStart'
 import { styled } from '@mui/material/styles'
 
 ace.config.set('basePath', '/node_modules/ace-builds/src-noconflict/')
@@ -43,9 +57,35 @@ export const ChartEditor: FunctionComponent<ChartEditorProps> = ({
       // eslint-disable-next-line no-new-func
       const func = new Function(
         'qsCreateCanvas',
+        'QsEnumColorScale',
+        'QsEnumCurve',
+        'QsEnumAlignmentBaseline',
+        'QsEnumTextAnchor',
+        'QsEnumTextFontWeight',
+        'QsEnumTextFontStyle',
+        'QsEnumTextDecorationLine',
+        'QsEnumTextFont',
+        'QsEnumLineJoin',
+        'QsEnumLineCap',
+        'QsEnumAxisScaleType',
+        'QsEnumScaleType',
         `return (function() { ${jsCode} })()`
       )
-      func(qsCreateCanvas)
+      func(
+        qsCreateCanvas,
+        QsEnumColorScale,
+        QsEnumCurve,
+        QsEnumAlignmentBaseline,
+        QsEnumTextAnchor,
+        QsEnumTextFontWeight,
+        QsEnumTextFontStyle,
+        QsEnumTextDecorationLine,
+        QsEnumTextFont,
+        QsEnumLineJoin,
+        QsEnumLineCap,
+        QsEnumAxisScaleType,
+        QsEnumScaleType
+      )
     } catch (execError) {
       console.log(execError)
     }

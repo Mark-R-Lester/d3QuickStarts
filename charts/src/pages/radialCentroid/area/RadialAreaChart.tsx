8f876265291adcfc5b09e30fc01168de7ab8d97a
mark-r-lester <mrlester@live.co.uk>
2025-06-11 14:13:30 +0100

    Fleshed out radial centroid area

--- charts/src/pages/radialCentroid/area/RadialAreaChart.tsx
+++ charts/src/pages/radialCentroid/area/RadialAreaChart.tsx
@@ -2,12 +2,20 @@ import { FunctionComponent, useEffect } from 'react'
 import { QsCanvas, qsCreateCanvas, QsRadialAreaData } from 'd3qs/d3QuickStart'
 import { ChartProps } from '../../../common/chartProps'
 
-export const RadialAreaElement: FunctionComponent<ChartProps> = ({
+export const RadialAreaChart: FunctionComponent<ChartProps> = ({
   canvasProps,
 }) => {
   useEffect(() => {
     const createChart = () => {
       const data1: QsRadialAreaData = {
+        outerData: [
+          15, 15, 15, 17, 16, 21, 14, 15, 16, 12, 15, 15, 15, 17, 16, 15, 15,
+          15, 17, 16, 21, 14, 15, 16, 12, 15,
+        ],
+        fillColor: 'lightBlue',
+      }
+
+      const data2: QsRadialAreaData = {
         innerData: [
           15, 15, 15, 17, 16, 21, 14, 15, 16, 12, 15, 15, 15, 17, 16, 15, 15,
           15, 17, 16, 21, 14, 15, 16, 12, 15,
@@ -16,11 +24,13 @@ export const RadialAreaElement: FunctionComponent<ChartProps> = ({
           16, 17, 18, 20, 17, 23, 23, 20, 17, 16, 16, 17, 18, 20, 17, 16, 17,
           18, 20, 17, 23, 23, 20, 17, 16, 16,
         ],
+        fillColor: 'darkBlue',
       }
 
       const canvas: QsCanvas = qsCreateCanvas(canvasProps)
 
       canvas.generate.radialCentroid.area(data1)
+      canvas.generate.radialCentroid.area(data2)
     }
     createChart()
   }, [canvasProps])
--- charts/src/pages/radialCentroid/area/RadialCentroidAreaPage.tsx
+++ charts/src/pages/radialCentroid/area/RadialCentroidAreaPage.tsx
@@ -2,46 +2,60 @@ import { Box, Typography } from '@mui/material'
 
 import { useState } from 'react'
 import { ChartButtonGrid } from '../../../components/molecules/ChartButtonGrid'
-import { RadialAreaElement } from './RadialArea'
+import { ConfigAndData } from '../../../components/atoms/chart/ConfigAndData'
+import { TryItYourSelf } from '../../../components/atoms/chart/TryItYourSelf'
+import {
+  defaultsContent,
+  configContent,
+  configAndData,
+  editorContent,
+} from './Content'
+import { RadialAreaDefaultsChart } from './RadialAreaDefaultsChart'
+import { RadialAreaChart } from './RadialAreaChart'
 
 export default function RadialCentroidAreaPage() {
   const menuElements: JSX.Element[] = [
-    <RadialAreaElement
+    <RadialAreaDefaultsChart
       canvasProps={{
-        chartName: 'radialArea',
+        chartName: 'radialAreaDefaultsChart',
         width: 130,
         lowestViewableValue: 0,
         highestViewableValue: 25,
       }}
     />,
-  ]
-
-  const charts: JSX.Element[] = [
-    <RadialAreaElement
+    <RadialAreaChart
       canvasProps={{
-        chartName: 'chart',
-        width: 800,
+        chartName: 'radialAreaChart',
+        width: 130,
         lowestViewableValue: 0,
         highestViewableValue: 25,
       }}
     />,
+    <ConfigAndData />,
+    <TryItYourSelf />,
   ]
 
-  const [chart, setChart] = useState<JSX.Element>(charts[0])
+  const contents: JSX.Element[] = [
+    defaultsContent,
+    configContent,
+    configAndData,
+    editorContent,
+  ]
+  const [content, setContent] = useState<JSX.Element>(contents[0])
   const onClick = (index: number) => {
-    setChart(charts[index])
+    setContent(contents[index])
   }
 
   return (
     <>
       <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-        Radial Centroid Area
+        Radial
       </Typography>
       <ChartButtonGrid
         onClick={onClick}
         elements={menuElements}
       ></ChartButtonGrid>
-      <Box>{chart}</Box>
+      <Box>{content}</Box>
     </>
   )
 }
